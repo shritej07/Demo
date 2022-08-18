b1ff7cb27c76df60f2b2c9333c0a6d80c3c80e8d
shritej07 <shritejnimbhorkar@gmail.com>
2022-08-18 15:55:46 +0530

    added math and calculator class

---
+++ src/basic/Math.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class Math {
+	
+    public static void add(int a, int b)
+    {
+        System.out.println("Summ is :" + a+b);
+    }
+    public static void add(int a, int b, int c)
+    {
+        System.out.println("Summ is :" + a+b+c);
+    }
+    public static void multiply(int a, int b)
+    {
+        System.out.println("Product is :" + a*b);
+    }
+
+}
